63e856a9e5974caaf5b953f5f54ea976bc8db4da
Iván Estruch <estruch95.B@gmail.com>
2015-10-03 00:35:39 +0200

    Signed-off-by: Iván Estruch <estruch95.B@gmail.com>

--- Proyecto 1e/src/StreamBytes.java
+++ Proyecto 1e/src/StreamBytes.java
@@ -7,21 +7,31 @@ import java.io.IOException;
 
 public class StreamBytes {
 	
+	//CREACIÓN DE ATRIBUTOS DE LA CLASE E INICIALIZADOS A NULL
 	private static FileInputStream fi=null;
 	private static FileOutputStream fo=null;
 	
 	public StreamBytes() {
 		// TODO Auto-generated constructor stub
+		//CONSTRUCTOR DE LA CLASE
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//EJECUCION DE METODOS EN MAIN
 		
+		//SI abrirFichero==true SE EJECUTARÁ EL MÉTODO copiaBytes
 		if(abrirFichero("lena.jpg","copiaLena.jpg")){
 			copiaBytes();
 			System.out.println("Fichero binario copiado correctamente.");
 		}
 	}
+	
+	/*Funcionalidad de "abrirFichero": Este método recibe dos parámetros que son el nombre del fichero origen y el nombre del fichero destino.
+	  Se generan dos objetos File con los nombres recibidos por parámetro y a continuación se comprueba que el fichero origen del cual queremos leer existe, si este existe,
+	  declaramos los atributos de la clase y retornamos TRUE, de lo contrario, retornamos FALSE.*/
+	
+	//Se capturan todas las excepciones con el encapsulado try/catch
 	
 	public static boolean abrirFichero(String nombreOrigen, String nombreDestino){
 		
@@ -30,6 +40,8 @@ public class StreamBytes {
 		
 		try{
 			if(origen.exists()){
+				//FileInputStream: Clase de Java para realizar lectura de bytes (Archivos binarios)
+				//FileOutputStream: Clase de Java para realizar escritura de bytes (Archivos binarios)
 				fi = new FileInputStream(origen);
 				fo = new FileOutputStream(destino);
 				return true;

0a238af6c78a7e116c4b24f7a7abe8601f838f5d
Iván Estruch <estruch95.B@gmail.com>
2015-10-03 00:24:18 +0200

    Proyecto 1e (Faltan comentarios)
    Signed-off-by: Iván Estruch <estruch95.B@gmail.com>

--- Proyecto 1e/src/StreamBytes.java
+++ Proyecto 1e/src/StreamBytes.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -5,60 +6,72 @@ import java.io.IOException;
 
 
 public class StreamBytes {
-
+	
+	private static FileInputStream fi=null;
+	private static FileOutputStream fo=null;
+	
 	public StreamBytes() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		copiaBytes();
+		
+		if(abrirFichero("lena.jpg","copiaLena.jpg")){
+			copiaBytes();
+			System.out.println("Fichero binario copiado correctamente.");
+		}
 	}
 	
-	public void abrirFichero(){
+	public static boolean abrirFichero(String nombreOrigen, String nombreDestino){
 		
+		File origen = new File(nombreOrigen);
+		File destino = new File(nombreDestino);
+		
+		try{
+			if(origen.exists()){
+				fi = new FileInputStream(origen);
+				fo = new FileOutputStream(destino);
+				return true;
+			}
+			else{
+				System.err.println("No existe el fichero binario espicificado.");
+			}
+		}
+		catch(SecurityException noExiste){
+			noExiste.printStackTrace();
+		}
+		catch (FileNotFoundException noExiste) {
+			noExiste.printStackTrace();
+		}
+		return false;
+				
 	}
 	
 	public static void copiaBytes(){
 		
-		FileInputStream fi = null;
-		FileOutputStream fo = null;
+		byte[] buffer = new byte[1024*1024];
+		int cadena;
 		
 		try {
-			fi = new FileInputStream("lena.jpg");
-			fo = new FileOutputStream("lena1Copia.jpg");
-			
-			byte [] buffer = new byte[1024*1024];
-			int cadena;
-			
 			while ((cadena = fi.read(buffer)) > 0) {
 				fo.write(buffer, 0, cadena);
-				cadena++;
 			}
-			System.out.println("Se han copiado: "+cadena+" bytes.");
 		}
-		catch (FileNotFoundException error) {
+		catch (IOException errorEscritura) {
 			// TODO Auto-generated catch block
-			error.printStackTrace();
-			System.err.println("Error al encontrar los ficheros.");
-		}
-		catch (IOException error) {
-			// TODO Auto-generated catch block
-			error.printStackTrace();
-			System.err.println("Error al realizar la lectura y la escritura.");
+			errorEscritura.printStackTrace();
 		}
 		finally{
 			try {
 				fi.close();
 				fo.close();
 			}
-			catch (IOException error) {
+			catch (IOException e) {
 				// TODO Auto-generated catch block
-				error.printStackTrace();
-				System.err.println("Error al cerrar los Streams.");
+				e.printStackTrace();
 			}
-		}		
-		
+		}
 	}
 
 }
